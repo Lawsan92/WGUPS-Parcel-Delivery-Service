77d8d8ae3f7f58991cfaeb27401709e46a5f270b
Lawrence <lawrencesanzogni@gmail.com>
2025-07-25 18:11:50 -0500

    Interface and main methods split. Interface now prints truck status based on input times

--- interface.py
+++ interface.py
@@ -1,18 +1,65 @@
+import datetime
+import csv
+
+import truck
+import package
+import distance_hash
+
+
 class Interface:
 
-    def get_itinerary_status (self, time, truck1, truck2, truck3):
+    def get_itinerary_status (self, packages):
 
-        for package in truck1:
-            if package.delivery_time >= time:
-                package.delivery_status = 'en route'
-                package.delivery_time = None
+        print('ENTER HOUR: ')
+        user_hour = int(input())
+        print('ENTER MINUTES: ')
+        user_minute = int(input())
 
-        for package in truck2:
-            if package.delivery_time >= time:
-                package.delivery_status = 'en route'
-                package.delivery_time = None
+        # initialize distance table
+        distances = distance_hash.DistanceHash()
+        distances.create_hash()
 
-        for package in truck3:
-            if package.delivery_time >= time:
-                package.delivery_status = 'en route'
-                package.delivery_time = None
+        # filled distances table from triangular matrix to symmetric table, this will make distance look up easier
+        distances.fill_hash()
+
+        # initialize trucks and manually load up default package ids
+        truck_1 = truck.Truck(1, [1, 13, 14, 15, 16, 20, 40, 2, 4, 5, 7, 8, 11, 12, 21, 23],
+                              datetime.timedelta(hours=8), 7, datetime.timedelta(hours=user_hour, minutes=user_minute))
+        truck_2 = truck.Truck(2, [3, 6, 36, 39, 25, 29, 30, 31, 32, 34, 37, 17, 18, 19, 38, 28],
+                              datetime.timedelta(hours=9, minutes=5), 7, datetime.timedelta(hours=user_hour, minutes=user_minute))
+        truck_3 = truck.Truck(3, [], datetime.timedelta(hours=user_hour, minutes=user_minute), 0, datetime.timedelta(hours=user_hour, minutes=user_minute))
+
+        # Read package data from csv file
+        with open('WGUPS Package File_edited.csv') as package_file_csv:
+            read_package_csv = csv.reader(package_file_csv, delimiter=',')
+
+            # skip file headers
+            next(read_package_csv)
+
+            for row in read_package_csv:
+                # initialize package
+                truck_package = package.Package(int(row[0]), row[1], row[2], row[4], row[5], row[6], row[7],
+                                                'at the hub')
+                # add package to package hash table
+                packages.insert_package(truck_package)
+
+        # load trucks with packages
+        truck_1.load_truck(packages.packages)
+        truck_2.load_truck(packages.packages)
+        truck_3.load_truck(packages.packages)
+
+        # deliver packages
+        truck_1.deliver_package(distances.hash)
+        truck_2.deliver_package(distances.hash)
+        if truck_1.inventory == 0:
+            truck_3.set_time(truck_1.get_time())
+            truck_3.deliver_package(distances.hash)
+
+        # print truck contents after completing deliveries
+        print('PRINTING TRUCKS AFTER COMPLETING DELIVERIES')
+        print(truck_1)
+        print(truck_2)
+        print(truck_3)
+
+        # print total miles
+        print('total miles:', truck_1.mileage + truck_2.mileage + truck_3.mileage)
--- main.py
+++ main.py
@@ -17,60 +17,61 @@ distances.create_hash()
 # filled distances table from triangular matrix to symmetric table, this will make distance look up easier
 distances.fill_hash()
 
-# initialize trucks and manually load up default package ids
-truck_1 = truck.Truck(1, [1, 13, 14, 15, 16, 20, 40, 2, 4, 5, 7, 8, 11, 12, 21, 23], datetime.timedelta(hours=8), 7)
-truck_2 = truck.Truck(2, [3, 6, 36, 39, 25, 29, 30, 31, 32, 34, 37, 17, 18, 19, 38, 28], datetime.timedelta(hours=9, minutes=5), 7)
-truck_3 = truck.Truck(3, [], datetime.timedelta(hours=9, minutes=5))
+print('GO TO INTERFACE (enter: 1), PRINT TOTAL MILEAGE (enter: 0):')
+user_input = input()
+if user_input == '1':
+    interface = interface.Interface()
+    interface.get_itinerary_status(packages)
+elif user_input == '0':
+    # initialize trucks and manually load up default package ids
+    truck_1 = truck.Truck(1, [1, 13, 14, 15, 16, 20, 40, 2, 4, 5, 7, 8, 11, 12, 21, 23], datetime.timedelta(hours=8), 7)
+    truck_2 = truck.Truck(2, [3, 6, 36, 39, 25, 29, 30, 31, 32, 34, 37, 17, 18, 19, 38, 28], datetime.timedelta(hours=9, minutes=5), 7)
+    truck_3 = truck.Truck(3, [], datetime.timedelta(hours=9, minutes=5))
 
-# Read package data from csv file
-with open('WGUPS Package File_edited.csv') as package_file_csv:
-    read_package_csv = csv.reader(package_file_csv, delimiter=',')
+    # Read package data from csv file
+    with open('WGUPS Package File_edited.csv') as package_file_csv:
+        read_package_csv = csv.reader(package_file_csv, delimiter=',')
 
-    # skip file headers
-    next(read_package_csv)
+        # skip file headers
+        next(read_package_csv)
 
-    for row in read_package_csv:
-        # initialize package
-        truck_package = package.Package(int(row[0]), row[1], row[2], row[4], row[5], row[6], row[7], 'at the hub')
-        # add package to package hash table
-        packages.insert_package(truck_package)
+        for row in read_package_csv:
+            # initialize package
+            truck_package = package.Package(int(row[0]), row[1], row[2], row[4], row[5], row[6], row[7], 'at the hub')
+            # add package to package hash table
+            packages.insert_package(truck_package)
 
-# load trucks with packages
-truck_1.load_truck(packages.packages)
-truck_2.load_truck(packages.packages)
-truck_3.load_truck(packages.packages)
+    # load trucks with packages
+    truck_1.load_truck(packages.packages)
+    truck_2.load_truck(packages.packages)
+    truck_3.load_truck(packages.packages)
 
-# PRINT TRUCKS BEFORE DEPARTURE
-print('PRINTING TRUCKS AT DEPARTURE TIME')
-print(truck_1)
-print(truck_2)
+    # PRINT TRUCKS BEFORE DEPARTURE
+    print('PRINTING TRUCKS AT DEPARTURE TIME')
+    print(truck_1)
+    print(truck_2)
 
-# deliver packages
-truck_1.deliver_package(distances.hash)
-truck_2.deliver_package(distances.hash)
-if truck_1.inventory == 0:
-    truck_3.set_time(truck_1.get_time())
-    print(truck_3) # <- updates truck 3's departure time
-    truck_3.deliver_package(distances.hash)
+    # deliver packages
+    truck_1.deliver_package(distances.hash)
+    truck_2.deliver_package(distances.hash)
+    if truck_1.inventory == 0:
+        truck_3.set_time(truck_1.get_time())
+        print(truck_3) # <- updates truck 3's departure time
+        truck_3.deliver_package(distances.hash)
 
-# print truck contents after completing deliveries
-print('PRINTING TRUCKS AFTER COMPLETING DELIVERIES')
-print(truck_1)
-print(truck_2)
-print(truck_3)
+    # print truck contents after completing deliveries
+    print('PRINTING TRUCKS AFTER COMPLETING DELIVERIES')
+    print(truck_1)
+    print(truck_2)
+    print(truck_3)
 
-# print total miles
-print('total miles:', truck_1.mileage + truck_2.mileage + truck_3.mileage)
+    # print total miles
+    print('total miles:', truck_1.mileage + truck_2.mileage + truck_3.mileage)
 
 # # user interface
 # print('GET TRUCKS DELIVERY STATUSES')
 # interface = interface.Interface()
-# print('ENTER HOUR: ')
-# user_hour = int(input())
-# print('ENTER MINUTES: ')
-# user_minute = int(input())
-# user_time = datetime.timedelta(hours=user_hour, minutes=user_minute)
-# interface.get_itinerary_status(user_time, truck_1.delivered_packages, truck_2.delivered_packages, truck_3.delivered_packages)
+# interface.get_itinerary_status()
 #
 #
 # # Check truck contents
